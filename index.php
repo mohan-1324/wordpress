d6aa72135da483ac69188de25f673713540ea244
mohan-1324 <109534372+mohan-1324@users.noreply.github.com>
2022-11-21 21:10:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
-    $nameErr = "Name is harini";
+    $nameErr = "Name should be capital";
   } else {
     $name = test_input($_POST["name"]);
     // check if name only contains letters and whitespace
