66c43d42b4253038b861a88c133a92d190de8173
mohan-1324 <109534372+mohan-1324@users.noreply.github.com>
2022-11-22 15:14:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
   
   if (empty($_POST["email"])) {
-    $emailErr = "Email is mandt";
+    $emailErr = "Email is mohan";
   } else {
     $email = test_input($_POST["email"]);
     // check if e-mail address is well-formed
