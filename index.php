0682fb54c60466bcf1c50d4cbee79f2dd91ac372
mohan-1324 <109534372+mohan-1324@users.noreply.github.com>
2022-11-29 12:46:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -26,12 +26,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
   
   if (empty($_POST["email"])) {
-    $emailErr = "Email is Harini";
+    $emailErr = "Email is invalid new";
   } else {
     $email = test_input($_POST["email"]);
     // check if e-mail address is well-formed
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
+      $emailErr = "Invalid emailss format";
     }
   }
     
