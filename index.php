1c8df24a863ccf19108e5b08c423bf9888ad65b3
mohan-1324 <109534372+mohan-1324@users.noreply.github.com>
2023-05-30 16:20:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ echo $gender;
 <?php
 
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
+ * Front to the WordPress application. This file doesn't do anything, but 
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress
